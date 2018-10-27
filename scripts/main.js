27748297cb604045be5eda120e8f95eccf4ea20a
Aria Moraine <ariamoraine@gmail.com>
2018-10-27 00:09:29 -0400

    fixed logout and added profile viewer

--- scripts/main.js
+++ scripts/main.js
@@ -1,11 +1,33 @@
 const popupUri = 'https://solid.github.io/solid-auth-client/dist/popup.html';
 $('#login button').click(() => solid.auth.popupLogin({ popupUri }));
+$('#logout button').click(() => solid.auth.logout())
 
 solid.auth.trackSession(session => {
     const loggedIn = !!session;
+
     $('#login').toggle(!loggedIn);
     $('#logout').toggle(loggedIn);
     $('#user').text(session && session.webId);
+
+    if (session) {
+        $('user').text(session.webId);
+        if (!$('#profile').val()) {
+            $('#profile').val(session.webId);
+        }
+    }
 });
 
-$('logout button').click(() => solid.auth.logout());
+const FOAF = $rdf.Namespace('http://xmlns.com/foaf/0.1/');
+$('#view').click(async function loadProfile() {
+  // Set up a local data store and associated data fetcher
+  const store = $rdf.graph();
+  const fetcher = new $rdf.Fetcher(store);
+
+  // Load the person's data into the store
+  const person = $('#profile').val();
+  await fetcher.load(person);
+
+  // Display their details
+  const fullName = store.any($rdf.sym(person), FOAF('name'));
+  $('#fullName').text(fullName && fullName.value);
+});
